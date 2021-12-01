7941d413227bf14d45f54b6c01b87581b8b335eb
Endriyani Rahayu <90310081+endriyanira@users.noreply.github.com>
2021-12-01 14:15:40 +0700

    feat: implementation frontend class and func components with react (#14)
    * feat: implementation frontend class components and functional components with react
    
    * feat: update readme.md
    
    * feat: update readme.md v1.3
    
    Co-authored-by: Endriyani Rahayu <endriyanir@gmail.com>

---
+++ frontend/src/views/Home/index.js
@@ -0,0 +1,126 @@
+import React from "react";
+import listItems from "../../items.json";
+import List from "../../components/List/index";
+import "./index.css";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Fab } from "@material-ui/core";
+import ViewStreamIcon from '@mui/icons-material/ViewStream';
+ 
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        shopItems: listItems,
+        cartItems: [],
+        cartHidden: true,
+        balance: 120,
+        };
+    }
+    kuranginBalance = (price) => {
+        this.state.balance -= price;
+        if(this.state.balance<0) {
+            alert("Balance is not sufficient");
+            this.state.balance += price;
+            return false;
+        }
+        return true;
+    }
+
+    tambahBalance = (price) => {
+        this.state.balance += price;
+    }
+
+    handleAddItemToCart = (item) => {
+        const balanceCukup = this.kuranginBalance(item.price);
+            if(balanceCukup === true) {
+                const newItems = [...this.state.cartItems];
+                const newItem = { ...item };
+                const targetInd = newItems.findIndex((it) => it.id === newItem.id);
+                if (targetInd < 0) {
+                    newItem.inCart = true;
+                    newItems.push(newItem);
+                    this.updateShopItem(newItem, true);
+                }
+            this.setState({ cartItems: newItems });
+        }
+    }
+
+    handleDeleteItemFromCart = (item) => {
+        this.tambahBalance(item.price)
+        const newItems = [...this.state.cartItems];
+        const newItem = { ...item };
+        const targetInd = newItems.findIndex((it) => it.id === newItem.id);
+
+        if(targetInd >= 0) {
+            newItem.inChart = false;
+            newItems.splice(targetInd, 1);
+            this.updateShopItem(newItem, false);
+        }
+        this.setState({ cartItems: newItems });
+    }
+
+    handleToggle = () => {
+        const cartHidden = this.state.cartHidden;
+        this.setState({ cartHidden: !cartHidden });
+        };
+
+
+    updateShopItem = (item, inCart) => {
+        const tempShopItems = this.state.shopItems;
+        const targetInd = tempShopItems.findIndex((it) => it.id === item.id);
+        tempShopItems[targetInd].inCart = inCart;
+        this.setState({ shopItems: tempShopItems });
+        };
+
+    
+    render() {
+        return (
+        <div className="container-fluid">
+            <h1 className="text-center mt-3 mb-0">Mini Commerce</h1>
+            <div style={{ position: "fixed", top: 25, right: 25 }}>
+            <Fab variant="extended" onClick={this.handleToggle}>
+                {this.state.cartHidden ? (
+                <Badge
+                    color="secondary"
+                    badgeContent={this.state.cartItems.length}
+                >
+                    <ShoppingCartIcon />
+                </Badge>
+                ) : (
+                <ViewStreamIcon />
+                )}
+            </Fab>
+            </div>
+            <p className="text-center text-secondary text-sm font-italic">
+            (this is a <strong>class-based</strong> application)
+            </p>
+            <p className="text-center text-primary">
+            Your Balance: <b> {this.state.balance}</b>{" "}
+            </p>
+            <div className="container pt-3">
+            <div className="row mt-3">
+                {!this.state.cartHidden ? (
+                <div className="col-sm">
+                    <List
+                    title="My Cart"
+                    items={this.state.cartItems}
+                    onItemClick={this.handleDeleteItemFromCart}
+                    ></List>
+                </div>
+                ) : (
+                <div className="col-sm">
+                    <List
+                    title="List Items"
+                    items={this.state.shopItems}
+                    onItemClick={this.handleAddItemToCart}
+                    isShopList={true}
+                    ></List>
+                </div>
+                )}
+            </div>
+            </div>
+        </div>
+        );
+    }
+}
